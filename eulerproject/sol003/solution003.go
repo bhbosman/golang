da2ad73a1aec68937145fd217c79c2f93719dd1e
Brendan Bosman <bhbosman@gmail.com>
2016-08-30 10:52:59 +0200

    look at solutions and fix some

--- eulerproject/sol003/solution003.go
+++ eulerproject/sol003/solution003.go
@@ -83,6 +83,36 @@ func AtkinsSievePrime(number uint64) []uint64 {
 }
 
 func LargestPrimeFactor(number uint64) uint64 {
+
+	lastFactor := uint64(1)
+	if number%2 == 0 {
+		lastFactor = 2
+		number = number / 2
+		for number%2 == 0 {
+			number = number / 2
+		}
+	}
+
+	factor := uint64(3)
+	maxFactor := uint64(math.Sqrt(float64(number)))
+
+	for (number > 1) && (factor < maxFactor) {
+		if number%factor == 0 {
+			number = number / factor
+			for number%factor == 0 {
+				number = number / factor
+			}
+			maxFactor = uint64(math.Sqrt(float64(number)))
+		}
+		factor = factor + 2
+	}
+	if number == 1 {
+		return lastFactor
+	}
+	return number
+}
+
+func MyLargestPrimeFactor(number uint64) uint64 {
 	if number%2 == 0 {
 		return LargestPrimeFactor(number / 2)
 	}
