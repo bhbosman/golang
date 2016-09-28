f66418ff83a9d6dc84d64a6287043a3ba6c4cd98
Bosman, Brendan Hermanus: Barclays (JHB) <Brendanhermanus.bosman@barclayscapital.com>
2016-09-28 09:54:11 +0200

    Euler Project Solution 22

--- eulerproject/solutions/solution022_test.go
+++ eulerproject/solutions/solution022_test.go
@@ -1,1 +1,42 @@
 package solutions
+
+import (
+	"encoding/csv"
+	"os"
+	"sort"
+	"testing"
+)
+
+//
+// https://projecteuler.net/problem=22
+//
+
+func TestSolution22_01(t *testing.T) {
+	file, err := os.Open("solution022_names.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	data, err := reader.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	sort.Strings(data)
+
+	NameScore := func(name string) int {
+		sum := 0
+		for _, v := range name {
+			sum += int(1 + v - 'A')
+		}
+		return sum
+	}
+	sum := 0
+	for i, v := range data {
+		sum += ((i + 1) * NameScore(v))
+	}
+	if 871198282 != sum {
+		t.Fail()
+	}
+}
